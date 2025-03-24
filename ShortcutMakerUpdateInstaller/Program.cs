b717f9bb26a891c18b4a3973ae73fffc2bd472d8
rutokuba <rutokuba@gmail.com>
2025-03-24 01:23:38 +0100

    Test 2

---
+++ ShortcutMakerUpdateInstaller/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Net;
+
+string message = "Start\n";
+try
+{
+    string githubProjectLink = "https://github.com/RekenGit/ShortcutMaker/tree/master/ShortcutMaker/UpdateFiles/";
+    string[] filesToUpdate = { "ShortcutMakerUpdate.deps.json", "ShortcutMakerUpdate.dll", "ShortcutMakerUpdate.exe", "ShortcutMakerUpdate.pdb", "ShortcutMakerUpdate.runtimeconfig.json" };
+    string[] filesToDownload = { "Microsoft.Win32.SystemEvents.dll", "ShortcutMaker.deps.json", "ShortcutMaker.dll", "ShortcutMaker.exe", "ShortcutMaker.pdb", "ShortcutMaker.runtimeconfig.json", "System.Drawing.Common.dll", "System.Private.Windows.Core.dll", "runtimes/win/lib/net8.0/Microsoft.Win32.SystemEvents.dll" };
+
+
+    var c = new WebClient();
+    c.DownloadProgressChanged += (se, e) =>
+    {
+        Console.Clear();
+        Console.Write($"{e.BytesReceived / 1024 / 1024}MB / {e.TotalBytesToReceive / 1024 / 1024}MB");
+    };
+    message += "#1\n";
+    foreach (string file in filesToUpdate)
+        File.Delete(Directory.GetCurrentDirectory() + $"\\{file}");
+    message += "#2\n";
+
+    var filePath = Directory.GetCurrentDirectory() + "\\";
+    foreach (string file in filesToDownload)
+    {
+        message += $"Downloading file...  {githubProjectLink + file}\n";
+        await c.DownloadFileTaskAsync(githubProjectLink + file, filePath + file);
+    }
+
+    try { Process.Start(filePath + "ShortcutMaker.exe"); } catch { }
+    message += "#4\n";
+    Process.GetCurrentProcess().Kill();
+}
+catch (Exception e) 
+{
+    using (StreamWriter sw = new("error.txt"))
+    {
+        sw.WriteLine(message);
+        sw.WriteLine("\n-----------------------------------------------------------------------------------\n");
+        sw.WriteLine(e.Message);
+        sw.WriteLine("\n-----------------------------------------------------------------------------------\n");
+        sw.WriteLine(e.StackTrace);
+    }
+}
